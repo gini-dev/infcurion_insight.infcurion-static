4296099280e39515fc1c49a01519614860f70b7d
h.minori <164137998+minori-web241@users.noreply.github.com>
2026-05-11 16:52:01 +0900

    Updated/Added simply-static-1-1778484835

---
+++ wp-content/plugins/duracelltomi-google-tag-manager/js/analytics-talk-content-tracking.js
@@ -0,0 +1,13 @@
+if("undefined"==typeof console){window.console={log:function(){}};}
+(function(){const debugMode=gtm4wp_scrollerscript_debugmode;const callBackTime=gtm4wp_scrollerscript_callbacktime;const readerLocation=gtm4wp_scrollerscript_readerlocation;let timer=0;let scroller=false;let endContent=false;let didComplete=false;const startTime=new Date();const beginning=startTime.getTime();let totalTime=0;if(!debugMode){window[gtm4wp_datalayer_name].push({'event':'gtm4wp.reading.articleLoaded'});}else{console.log('Article loaded');}
+function trackLocation(){const bottom=window.innerHeight+window.scrollY;const height=document.body.offsetHeight
+let scrollStart=0;if(bottom>readerLocation&&!scroller){const currentTime=new Date();scrollStart=currentTime.getTime();const timeToScroll=Math.round((scrollStart-beginning)/ 1000);if(!debugMode){window[gtm4wp_datalayer_name].push({'event':'gtm4wp.reading.startReading','timeToScroll':timeToScroll});}else{console.log('Started reading '+timeToScroll);}
+scroller=true;}
+const scroll_top=document.querySelector('#'+gtm4wp_scrollerscript_contentelementid)?.scrollTop||0;const client_height=document.querySelector('#'+gtm4wp_scrollerscript_contentelementid)?.clientHeight||0;if(bottom>=scroll_top+client_height&&!endContent){const currentTime=new Date();const contentScrollEnd=currentTime.getTime();const timeToContentEnd=Math.round((contentScrollEnd-scrollStart)/ 1000);if(!debugMode){window[gtm4wp_datalayer_name].push({'event':'gtm4wp.reading.contentBottom','timeToScroll':timeToContentEnd});}else{console.log('End content section '+timeToContentEnd);}
+endContent=true;}
+if(bottom>=height&&!didComplete){const currentTime=new Date();const end=currentTime.getTime();totalTime=Math.round((end-scrollStart)/ 1000);if(!debugMode){if(totalTime<gtm4wp_scrollerscript_scannertime){window[gtm4wp_datalayer_name].push({'event':'gtm4wp.reading.readerType','readerType':'scanner'});}else{window[gtm4wp_datalayer_name].push({'event':'gtm4wp.reading.readerType','readerType':'reader'});}
+window[gtm4wp_datalayer_name].push({'event':'gtm4wp.reading.pagebottom','timeToScroll':totalTime});}else{if(totalTime<gtm4wp_scrollerscript_scannertime){console.log('The visitor seems to be a "scanner"');}else{console.log('The visitor seems to be a "reader"');}
+console.log('Bottom of page '+totalTime);}
+didComplete=true;}}
+document.addEventListener("scroll",function(event){if(timer){clearTimeout(timer);}
+timer=setTimeout(trackLocation,callBackTime);});})(window,document);
