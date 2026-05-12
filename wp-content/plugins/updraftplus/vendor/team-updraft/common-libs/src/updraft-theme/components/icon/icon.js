0c5e5722b929b8546e84aad453c2dbaf8d50a04d
h.minori <164137998+minori-web241@users.noreply.github.com>
2026-05-12 09:52:43 +0900

    Updated/Added simply-static-1-1778545592

---
+++ wp-content/plugins/updraftplus/vendor/team-updraft/common-libs/src/updraft-theme/components/icon/icon.js
@@ -0,0 +1,2 @@
+import{TUHTMLElement}from"../../helpers/tu-html-element";class TUIcon extends TUHTMLElement{constructor(){super();}}
+customElements.define("tu-icon",TUIcon);
---
+++ wp-content/plugins/updraftplus/vendor/team-updraft/common-libs/src/updraft-theme/theme.js
@@ -0,0 +1,1 @@
+import"./components/button/button";import"./components/icon/icon";
